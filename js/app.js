a0ea43b974e1c7d8e80303436db7075083b4b0f0
Leonardo Esquivel <leo22esquivel@gmail.com>
2021-11-17 21:07:48 -0300

    componentes

---
+++ js/app.js
@@ -0,0 +1,18 @@
+Vue.component('primer-componete',{
+    template:`<h1>hola mundo</h1>`
+})
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+     
+    
+    },
+   
+
+   
+    
+   
+    
+  })
